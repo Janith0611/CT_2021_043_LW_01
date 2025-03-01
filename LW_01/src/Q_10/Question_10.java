d698e1e564ccc1836ba73239f9fdee2b32baa4e4
Janith0611 <janithwaththegedara@gmail.com>
2025-03-01 21:32:38 +0530

    Add files via upload
    This is the first Java Lab Practical...

---
+++ LW_01/src/Q_10/Question_10.java
@@ -0,0 +1,18 @@
+package Q_10;
+import java.util.Scanner;
+
+public class Question_10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an odd-length word:");
+        String input = scanner.nextLine();
+
+        if (input.length() % 2 == 1) {
+            int middleIndex = input.length() / 2;
+            char middleChar = input.charAt(middleIndex);
+            System.out.println(middleChar);
+        } else {
+            System.out.println("The input word does not have an odd length.");
+        }
+    }
+}
